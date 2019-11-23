c3ca941a14c8ca6428c960c19e6df87f2339eff5
Valery Davydkin <st.valeriq@gmail.com>
2019-11-24 03:59:35 +0700

    about

--- js/main.js
+++ js/main.js
@@ -4,7 +4,23 @@ $(document).ready(function () {
         prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
         nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
         fade: true,
-        dots: true
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: false
         
     });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0)
+            $('#toTop').fadeIn();
+        else
+            $('#toTop').fadeOut();
+    });
+    $('#toTop').click(function () {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 800);
+    });
+
 });
