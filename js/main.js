351b3ad0120c08519d21250cbde7495c952d1615
Valery Davydkin <st.valeriq@gmail.com>
2019-11-23 19:53:44 +0700

    head is done

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.header__slider').slick({
+        infinite: true,
+        prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+        nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+        fade: true,
+        dots: true
+        
+    });
+});
