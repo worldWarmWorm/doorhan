b40f87efab5c6f491332827ceeb4c940f0adf71c
Valery Davydkin <st.valeriq@gmail.com>
2019-11-24 22:51:38 +0700

    toggle buttons

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
+
+    // slider
     $('.header__slider').slick({
         infinite: true,
         prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
         nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
         fade: true,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 5000
+        dots: true
+        // autoplay: true,
+        // autoplaySpeed: 5000
         
     });
 
+    // to top
     $(window).scroll(function () {
         if ($(this).scrollTop() != 0)
             $('#toTop').fadeIn();
@@ -20,6 +23,24 @@ $(document).ready(function () {
         $('body, html').animate({
             scrollTop: 0
         }, 800);
+    });
+
+    // toggle buttons
+    $('.toggle-button_1').click(function () {
+        $('.item-1__content').toggle(300);
+
+        if ($('.toggle-button .show-icon')) {
+            $('.toggle-button .show-icon').hide();
+            $('.toggle-button .hide-icon').show();
+        } else {
+            $('.toggle-button .hide-icon').hide();
+            $('.toggle-button .show-icon').show();
+        }
+    });
+
+
+    $('.toggle-button_2').click(function () {
+        $('.item-2__content').toggle(300);
     });
 
 });
